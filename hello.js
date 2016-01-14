be7cd068c8041458d353055567ba8798d32da1d6
fangl <fangliang@qq.com>
2016-01-14 15:29:47 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World in Node!\n'); // 这里没有分号，将不会通过linting
+    res.end('Hello World in Node1!\n'); // 这里没有分号，将不会通过linting
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
